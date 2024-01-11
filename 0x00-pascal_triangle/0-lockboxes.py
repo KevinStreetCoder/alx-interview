8bd3105ba404f2af92bf45890749f25094360e92
Kevin Kareithi <kevin.kareithi@fsdafrica.org>
2024-01-11 18:59:02 +0300

    0x01. Lockboxes

---
+++ 0x00-pascal_triangle/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+    - boxes (list of lists): A list of boxes, where each box is represented as a list of keys.
+
+    Returns:
+    - bool: True if all boxes can be opened, False otherwise.
+    """
+    # Set to keep track of opened boxes
+    opened_boxes = set([0])
+
+    # List to keep track of boxes to be explored
+    boxes_to_explore = [0]
+
+    # Explore boxes
+    while boxes_to_explore:
+        current_box = boxes_to_explore.pop()
+
+        # Get keys in the current box
+        keys = boxes[current_box]
+
+        for key in keys:
+            # If the key opens a new box and that box hasn't been opened yet
+            if key not in opened_boxes and key < len(boxes):
+                opened_boxes.add(key)
+                boxes_to_explore.append(key)
+
+    # Check if all boxes have been opened
+    return len(opened_boxes) == len(boxes)
+
