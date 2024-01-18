13fc473f3763df816892b2b8de6221a92b4f6b46
Kevin Kareithi <kevin.kareithi@fsdafrica.org>
2024-01-18 19:12:12 +0300

    updates

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,60 +1,28 @@
- #!/usr/bin/python3
-'''Minimum Operations python3 challenge'''
-
+#!/usr/bin/python3
+"""
+0. Minimum Operations
+"""
 
 def minOperations(n):
-    '''calculates the fewest number of
-    operations needed to result in exactly n H
-    characters in this file.
-    Returns:
-        Integer : if n is impossible to achieve, return 0
-    '''
-    pasted_chars = 1  # how many chars in the file
-    clipboard = 0  # how many H's copied
-    counter = 0  # operations counter
+    """
+    Calculates the fewest number of operations needed to result in exactly n 'H' characters.
 
-    while pasted_chars < n:
-        # if did not copy anything yet
-        if clipboard == 0:
-            # copyall
-            clipboard = pasted_chars
-            # increment operations counter
-            counter += 1
-
-        # if haven't pasted anything yet
-        if pasted_chars == 1:
-            # paste
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 1
-            # continue to next loop
-            continue
-
-        remaining = n - pasted_chars  # remaining chars to Paste
-        # check if impossible by checking if clipboard
-        # has more than needed to reach the number desired
-        # which also means num of chars in file is equal
-        # or more than in the clipboard.
-        # in both situations it's impossible to achieve n of chars
-        if remaining < clipboard:
-            return 0
-
-        # if can't be devided
-        if remaining % pasted_chars != 0:
-            # paste current clipboard
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 1
-        else:
-            # copyall
-            clipboard = pasted_chars
-            # paste
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 2
-
-    # if got the desired result
-    if pasted_chars == n:
-        return counter
-    else:
+    :param n: Number of 'H' characters to achieve
+    :type n: int
+    :return: Minimum number of operations
+    :rtype: int
+    """
+    if n <= 1:
         return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+s
